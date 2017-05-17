871471f8381c73b54c22bd8139ba5a5d91e849e7
Baoli Zhai <horidream@gmail.com>
2017-05-17 23:39:15 +0800

    add more calayer function

--- Shifu/Classes/CALayerExtension.swift
+++ Shifu/Classes/CALayerExtension.swift
@@ -17,3 +17,48 @@ extension CALayer{
         return image
     }
 }
+
+extension CALayer: CAAnimationDelegate{
+    public func animationDidStart(_ anim: CAAnimation) {
+        if let onStart = self.value(forKey: "__shifu_on_start__") as? () -> Void{
+            onStart()
+        }
+    }
+    
+    public func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        if let onComplete = self.value(forKey: "__shifu_on_complete__") as? (Bool) -> Void{
+            onComplete(flag)
+        }
+        
+    }
+    
+    public func onAnimationComplete(_ callback:(Bool)->Void){
+        self.setValue(callback, forKey: "__shifu_on_complete__")
+    }
+    
+    public func onAnimaitonStart(_ callback:()->Void){
+        self.setValue(callback, forKey: "__shifu_on_start__")
+    }
+}
+
+extension CALayer:CALayerDelegate{
+    
+    public func action(for layer: CALayer, forKey event: String) -> CAAction? {
+        if event == kCAOnOrderIn, let callback = self.value(forKey:"__shifu_on_add_to_stage__") as? ()->Void{
+            callback()
+            if let action = layer.value(forKey: "__shifu_on_add_to_stage_action__") as? CAAction
+            {
+                return action
+            }
+        }
+        return nil
+    }
+    
+    public func onAddToStage(action:CAAction? = nil, _ callback:()->Void){
+        self.delegate = self
+        self.setValue(callback, forKey: "__shifu_on_add_to_stage__")
+        if let action = action{
+            self.setValue(action, forKey: "__shifu_on_add_to_stage_action__")
+        }
+    }
+}
