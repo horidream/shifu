3fb08393506eaae4653ef1f5964057f85f8aba17
Baoli Zhai <horidream@gmail.com>
2017-07-05 23:12:37 +0800

    clear

--- Example/Pods/Target Support Files/Pods-Shifu_Tests/Pods-Shifu_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Shifu_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_Shifu_Tests
-@end
--- Example/Pods/Target Support Files/Pods-Shifu_Tests/Pods-Shifu_Tests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_Shifu_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Shifu_TestsVersionString[];
-
