4139d6cd0a1051d3e2a4adef9bf59b5691803b8f
Horidream <Horidream@gmail.com>
2019-04-15 22:09:54 +0800

    clean up

--- Example/Pods/Target Support Files/Pods-Shifu_Example/Pods-Shifu_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Shifu_Example : NSObject
-@end
-@implementation PodsDummy_Pods_Shifu_Example
-@end
--- Example/Pods/Target Support Files/Shifu/Shifu-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Shifu : NSObject
-@end
-@implementation PodsDummy_Shifu
-@end
