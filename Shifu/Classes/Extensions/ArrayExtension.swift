2e170d033b04795e3d86477739519ab71e11f9e6
Horidream <Horidream@gmail.com>
2019-06-04 23:23:56 +0800

    add missing file, make function public

--- Shifu/Classes/Extensions/ArrayExtension.swift
+++ Shifu/Classes/Extensions/ArrayExtension.swift
@@ -10,7 +10,7 @@ import Foundation
 
 
 extension Array {
-    subscript (safe index: Int) -> Element? {
+    public subscript (safe index: Int) -> Element? {
         return index < count && index >= 0 ? self[index] : nil
     }
 }
