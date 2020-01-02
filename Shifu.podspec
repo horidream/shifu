ced86bd289d918871d50035c99e4fb692b3ce276
Hori <horidream@gmail.com>
2020-01-02 23:30:42 +0800

    udpate and clean up

--- Shifu.podspec
+++ Shifu.podspec
@@ -8,8 +8,8 @@
 
 Pod::Spec.new do |s|
   s.name             = 'Shifu'
-  s.version          = '0.1.8'
-  s.summary          = 'A short description of Shifu.'
+  s.version          = '0.1.9'
+  s.summary          = 'Shifu.'
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
--- Shifu/Classes/Extensions/UIImageExtension.swift
+++ Shifu/Classes/Extensions/UIImageExtension.swift
@@ -62,4 +62,33 @@ public extension UIImage{
     func resizedImage(scale: CGFloat, smoothing:Bool = false) -> UIImage?{
         return resizedImage(scaleX: scale, scaleY: scale, smoothing:smoothing)
     }
+    
+    func invertAlpha() -> UIImage? {
+        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
+        let (width, height) = (Int(self.size.width), Int(self.size.height))
+        let bytesPerPixel = 4
+        let bytesPerRow = bytesPerPixel * width
+        let byteOffsetToAlpha = 3 // [r][g][b][a]
+        if let context = CGContext(data: nil, width: width, height: height, bitsPerComponent: 8, bytesPerRow: bytesPerRow,
+                                   space: CGColorSpaceCreateDeviceRGB(), bitmapInfo: bitmapInfo.rawValue),
+            let cgImage = self.cgImage {
+            context.setFillColor(UIColor.clear.cgColor)
+            context.fill(CGRect(origin: CGPoint.zero, size: self.size))
+            context.draw(cgImage, in: CGRect(origin: CGPoint.zero, size: self.size))
+            if let memory: UnsafeMutableRawPointer = context.data {
+                for y in 0..<height {
+                    let pointer = memory.advanced(by: bytesPerRow * y)
+                    let buffer = pointer.bindMemory(to: UInt8.self, capacity: bytesPerRow)
+                    for x in 0..<width {
+                        let rowOffset = x * bytesPerPixel + byteOffsetToAlpha
+                        buffer[rowOffset] = 0xff - buffer[rowOffset]
+                    }
+                }
+                if let cgImage =  context.makeImage() {
+                    return UIImage(cgImage: cgImage)
+                }
+            }
+        }
+        return nil
+    }
 }
