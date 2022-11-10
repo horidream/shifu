46927148da69a9ecc58e4ccf0720ab3b5ea26b47
Hori <horidream@gmail.com>
2022-11-10 09:48:06 +0800

    add ci scripts

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+# fail if any command fails
+
+echo "🧩 Stage: Post-clone is activated .... "
+
+set -e
+# debug log
+set -x
+
+# Install dependencies using Homebrew. This is MUST! Do not delete.
+brew install node yarn cocoapods
+
+# Install yarn and pods dependencies.
+# If you're using Flutter or Swift
+# just install pods by "pod install" command
+cd ../web && yarn install && yarn build
+cd .. && pod install
+
+echo "🎯 Stage: Post-clone is done .... "
+
+exit 0
