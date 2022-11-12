4412b1c879f1a95703a920b3352349870410f064
Hori <horidream@gmail.com>
2022-11-12 20:31:54 +0800

    add relative path grogu dependency

--- Example/ci_scripts/ci_post_clone.sh
+++ Example/ci_scripts/ci_post_clone.sh
@@ -12,6 +12,8 @@ set -x
 # Install dependencies using Homebrew. This is MUST! Do not delete.
 brew install node yarn cocoapods
 
+git clone https://${GITHUB_TOKEN}:x-oauth-basic@github.com/horidream/grogu.git ../../../grogu
+
 # Install yarn and pods dependencies.
 # If you're using Flutter or Swift
 # just install pods by "pod install" command
